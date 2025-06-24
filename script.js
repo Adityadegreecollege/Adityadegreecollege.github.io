0f2d10707e2daa4c237713de9f4602fe250d901e
Adityadegreecollege <adityadegreecollege45@gmail.com>
2025-06-24 10:22:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+    // Remove loading animation after 1 second
+    window.onload = () => {
+      const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+        clearTimeout(c);
+      }, 1000);
+    };
+
+    // Redirect to another site after 7 seconds
+      window.onload = () => {
+      const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+        clearTimeout(c);
+      }, 1000);
+    };
+
+    // Redirect to another site after 7 seconds
+        window.onload = () => {
+      const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+        clearTimeout(c);
+      }, 1000);
+    };
+
+    // Redirect to another site after 7 seconds
+          setTimeout(() => {
+      window.location.href = "main.html"; // Your target HTML file
+    }, 6000);
+
+
